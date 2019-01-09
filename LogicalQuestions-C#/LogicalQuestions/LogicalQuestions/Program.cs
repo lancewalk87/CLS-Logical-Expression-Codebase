01d2865b90808a58a7a09257dbdce3c975e2089c
Lance Walker <walkerlance65@gmail.com>
2019-01-08 18:17:53 -0700

    v2.0.7

--- LogicalQuestions-C#/LogicalQuestions/LogicalQuestions/Program.cs
+++ LogicalQuestions-C#/LogicalQuestions/LogicalQuestions/Program.cs
@@ -2,23 +2,38 @@
 
 namespace LogicalQuestions {
     class Program {
-        private static string isPalindrome(string ent)
-        { // String Check
-            int leng = ent.Length; 
+        struct palindrome
+        {
+            public bool was;
+            public string msg; 
+        }
 
+        static palindrome getResult(string ent)
+        {
+            palindrome result = new palindrome { was = true, msg = "IS" };
+            int leng = ent.Length;
+            
             for (int i=0; i<leng/2; i++)
             {
-                if (ent[i] != ent[leng - 1 - i]) return "IS NOT"; 
+                if (ent[i] != ent[leng - 1 - i]) { result.was = false; result.msg = "IS NOT"; } 
             }
-            return "IS"; 
+
+            return result; 
         }
+
+       //  palindrome* test_results = null; 
 
         static void Main(string[] args) {
             Console.WriteLine("INIT: PALINDROME");
-            string[] test_cases = new string[] { "one", "two", "12121", "racecar", "mom", "test" };
+            
+            string[] test_cases = new string[] { "one", "12121", "mom", "racecar", "two" };
 
-            // index: test_cases 
-            foreach (var i in test_cases) Console.WriteLine("TESTING: {1}, {1} A PALINDROME.", i, isPalindrome(i));
+            // index: test_cases:
+           foreach (var i in test_cases)
+            {
+                palindrome res = getResult(i);
+                Console.WriteLine("TESTING: {0}, {1} A PALINDROME", i, res.msg); 
+            }
         }
     }
 }
